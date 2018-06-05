1c85b8c913f9070883d37b79bd2f015c6e05b142
tiagoluzh607 <tiagoluz.h607@gmail.com>
2018-06-05 16:59:58 -0300

    Fazendo um post do formulario somente se for valido e exibindo uma mensagem ao usuario

--- public/js/controllers/foto-controller.js
+++ public/js/controllers/foto-controller.js
@@ -4,12 +4,25 @@
  * and open the template in the editor.
  */
 
-angular.module('alurapic').controller('FotoController', function($scope){
+angular.module('alurapic').controller('FotoController', function($scope, $http){
     
     $scope.foto = {};
+    $scope.mensagem = '';
     
     $scope.submeter = function(){
-        console.log($scope.foto);
+        
+        if($scope.formulario.$valid){ // faz o post somente se o formulario for valido
+        
+            $http.post('v1/fotos', $scope.foto)
+                    .success(function(){
+                        $scope.mensagem = 'foto incluida com sucesso';
+                    })
+                    .error(function(erro){
+                        $scope.mensagem = 'Não foi possível incluir a foto';
+                    });
+        }
     };
+    
+    
     
 });
