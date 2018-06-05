0e552813d03a16ddb1c32c1163a7406f49dc26b3
tiagoluzh607 <tiagoluz.h607@gmail.com>
2018-06-05 14:58:05 -0300

    Trabalhando no sistema de rotas do angular, divindo o html em partes (partials) e direcionando para esse html

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,17 @@
-angular.module('alurapic', ['minhasDiretivas', 'ngAnimate']); //o modulo principal depende de minhasDiretivas
+angular.module('alurapic', ['minhasDiretivas', 'ngAnimate', 'ngRoute']) //o modulo principal depende de minhasDiretivas
+        .config(function($routeProvider){ // injeção do routeProvider atraves da dependencia ngRoute
+            
+            $routeProvider.when('/fotos', { //deve ser acessado asim /#/fotos
+                templateUrl: 'partials/principal.html',
+                controller: 'FotosController'
+            });
+            
+            $routeProvider.when('/fotos/new', { //deve ser acessado asim /#/fotos/new
+                templateUrl: 'partials/foto.html'
+            });
+            
+            
+            $routeProvider.otherwise({ redirectTo: '/fotos'});
+            
+            
+        });
